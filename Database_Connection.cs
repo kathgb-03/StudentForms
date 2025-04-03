ec74c886c844a3a4d0e779ab0b4e0778350ac671
Katherine Grace <missmatchaa11@gmail.com>
2025-04-03 19:22:35 +0800

    Add files via upload

---
+++ Database_Connection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+public class Database_Connection
+{
+    private string connectionString = "server=localhost; database=StudentInfoDB; uid=root; pwd=Keypass_2003;";
+
+    public MySqlConnection GetConnection()
+    {
+        return new MySqlConnection(connectionString);
+    }
+}
