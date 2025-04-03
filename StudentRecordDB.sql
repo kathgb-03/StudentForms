ff7eba86a6fde03bd623c6c29e6d327afed0e340
Katherine Grace <missmatchaa11@gmail.com>
2025-04-03 19:26:20 +0800

    Add files via upload

---
+++ StudentRecordDB.sql
@@ -0,0 +1,8 @@
+INSERT INTO StudentRecordTB 
+(studentId, firstName, lastName, middleName, houseNo, brgyName, municipality, province, region, country, birthdate, age, studContactNo, emailAddress, guardianFirstName, guardianLastName, hobbies, nickname, courseId, yearId)
+VALUES
+(1, 'Kath', 'Grace', 'L.', 89, 'Brgy San Roque', 'Quezon City', 'Metro Manila', 'NCR', 'Philippines', '2004-11-05', 20, '09171234567', 'kath.grace@email.com', 'Linda', 'Grace', 'Reading', 'Kathy', 2, 3),
+(2, 'Aira', 'Riego', 'E.', 104, 'Brgy Bagong Silang', 'Caloocan', 'Metro Manila', 'NCR', 'Philippines', '2003-06-15', 21, '09281234567', 'aira.riego@email.com', 'Jose', 'Riego', 'Singing', 'Ai', 1, 2),
+(3, 'Jazzy', 'Swift', 'R.', 21, 'Brgy Poblacion', 'Lipa City', 'Batangas', 'Region IV-A', 'Philippines', '2002-09-10', 22, '09391234567', 'jazzy.swift@email.com', 'Carmen', 'Swift', 'Playing Guitar', 'Jazz', 3, 4),
+(4, 'John', 'Bogart', 'D.', 715, 'Brgy Sta. Lucia', 'Pasig City', 'Metro Manila', 'NCR', 'Philippines', '1999-03-30', 25, '09456789012', 'john.bogart@email.com', 'David', 'Bogart', 'Cycling', 'Johnny', 4, 4),
+(5, 'Susan', 'Batumbakal', 'V.', 36, 'Brgy Kapitolyo', 'Mandaluyong City', 'Metro Manila', 'NCR', 'Philippines', '2003-07-19', 21, '09567890123', 'susan.batumbakal@email.com', 'Emily', 'Batumbakal', 'Painting', 'Sue', 1, 1);
